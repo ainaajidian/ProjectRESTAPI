b2c2d4f119ae35ee01b702ba07c1c428b9ffa04d
ainaajidian <ainaajidian@gmail.com>
2022-01-10 16:51:48 +0700

    Add Model and Migrations

---
+++ ProjectRESTAPI/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectRESTAPI.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ ProjectRESTAPI/Models/UserRole.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectRESTAPI.Models
+{
+    public class UserRole
+    {
+        public string UserName { get; set; }
+        public string RoleName { get; set; }
+    }
+}

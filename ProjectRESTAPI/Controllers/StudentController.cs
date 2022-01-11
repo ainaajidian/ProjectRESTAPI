bb7086ee0024c1a5655eee0f281c2cc55e10891c
ainaajidian <ainaajidian@gmail.com>
2022-01-11 09:55:15 +0700

    Add Controller

---
+++ ProjectRESTAPI/Controllers/StudentController.cs
@@ -0,0 +1,96 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProjectRESTAPI.Data;
+using ProjectRESTAPI.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ProjectRESTAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private IStudent _student;
+        private IMapper _mapper;
+
+        public StudentsController(IStudent student, IMapper mapper)
+        {
+            _student = student ?? throw new ArgumentNullException(nameof(student));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<StudentDto>>> Get()
+        {
+            var students = await _student.GetAll();
+
+            var dtos = _mapper.Map<IEnumerable<StudentDto>>(students);
+            return Ok(dtos);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<StudentDto>> Get(int id)
+        {
+            var result = await _student.GetById(id.ToString());
+            if (result == null)
+                return NotFound();
+
+            return Ok(_mapper.Map<StudentDto>(result));
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpPost]
+        public async Task<ActionResult<StudentDto>> Post([FromBody] StudentCreateDto studentforCreateDto)
+        {
+            try
+            {
+                var student = _mapper.Map<Models.Student>(studentforCreateDto);
+                var result = await _student.Insert(student);
+                var studentReturn = _mapper.Map<Dtos.StudentDto>(result);
+                return Ok(studentReturn);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<StudentDto>> Put(int id, [FromBody] StudentCreateDto studentForCreateDto)
+        {
+            try
+            {
+                var student = _mapper.Map<Models.Student>(studentForCreateDto);
+                var result = await _student.Update(id.ToString(), student);
+                var studentdto = _mapper.Map<Dtos.StudentDto>(result);
+                return Ok(studentdto);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                await _student.Delete(id.ToString());
+                return Ok($"Data student {id} berhasil didelete");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
+

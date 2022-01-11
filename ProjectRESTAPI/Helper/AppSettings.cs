a6183b73eaa6854d03d0343c217bfdddb4a1a8e5
ainaajidian <ainaajidian@gmail.com>
2022-01-11 09:54:53 +0700

    Add Helper, Validation and Profile

---
+++ ProjectRESTAPI/Helper/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace ProjectRESTAPI.Helper
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
---
+++ ProjectRESTAPI/Profiles/CoursesProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+
+namespace ProjectRESTAPI.Profiles
+{
+    public class CoursesProfile : Profile
+    {
+        public CoursesProfile()
+        {
+            CreateMap<Dtos.CourseCreateDto, Models.Course>(); 
+            CreateMap<Models.Course, Dtos.CourseCreateDto>();
+            CreateMap<Dtos.CourseDto, Models.Course>();
+            CreateMap<Models.Course, Dtos.CourseDto>();
+        }
+    }
+}
